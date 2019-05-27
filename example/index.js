f4e6b1e5cb24b8687beae8c3cc04f3e0ec20ff9f
Jefferson Paredes <jeff@comgtech.com>
2019-05-27 16:51:39 +0800

    Updated example json response

--- example/index.js
+++ example/index.js
@@ -11,25 +11,23 @@ var client = new SemaphoreSMS({
 });
 
 client.sendMessage({
-        number: '09123456789',
+        number: '09051234567',
         message: 'Hello World'
     }, (err, data) => {
     console.log(err || data);
     // =>
-    //      {
-    //      "message_id": "123456",
-    //      "user_id": "123456",
-    //      "user": "test@mail.com",
-    //      "account_id": "1234",
-    //      "account": "Test",
-    //      "recipient": "091234567",
-    //      "message": "Hello World",
-    //      "sender_name": "Test",
-    //      "network": "Smart",
-    //      "status": "Pending",
-    //      "source": "prepaid",
-    //      "type": "127.33",
-    //      "created_at": "2017-08-04T09:59:29.660Z",
-    //      "updated_at": "2017-08-05T09:59:29.660Z"
-    //      }
+    // [ { message_id: 72725061,
+    //     user_id: 7291,
+    //     user: 'username@yourdomain.com',
+    //     account_id: 7158,
+    //     account: 'AccountName',
+    //     recipient: '639051234567',
+    //     message: 'Hello World',
+    //     sender_name: 'Semaphore',
+    //     network: 'Globe',
+    //     status: 'Pending',
+    //     type: 'Single',
+    //     source: 'Api',
+    //     created_at: '2019-05-27 16:49:20',
+    //     updated_at: '2019-05-27 16:49:20' } ]
 });
